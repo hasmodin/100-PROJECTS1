fd43558981d8f3e2cdd033f724f97733f051ad52
Hasmodin <hasmodin@gmail.com>
2024-06-24 07:44:35 +0300

    updated

--- 29.Social-Media-Selector/app.js
+++ 29.Social-Media-Selector/app.js
@@ -3,12 +3,12 @@
 const selectField = document.querySelector("#selectfield");
 const list = document.querySelector("#list");
 const options = document.querySelectorAll(".options");
-console.log(options);
+
 const selectTxt = document.querySelector("#slectText");
 let icon = document.querySelector("#arrowicon");
 
 selectField.addEventListener("click", () => {
-    console.log("button was clicked");
+    // console.log("button was clicked");
     
     list.classList.toggle("hide");
     icon.classList.toggle("rotate");
